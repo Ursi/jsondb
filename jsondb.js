b445a31e3ce8dcc8769c910acc05e2327159f8c0
Mason Mackaman <masondeanm@aol.com>
2019-07-16 13:26:13 -0400

    Add restore from backup functionality

--- jsondb.js
+++ jsondb.js
@@ -15,21 +15,29 @@ module.exports = {
 	async get(dataPath) {
 		dataPath = addJson(dataPath);
 		let fullPath = path.join(this.basePath, dataPath);
-	    return JSON.parse(await fsp.readFile(fullPath));
+		try {
+			return JSON.parse(await fsp.readFile(fullPath));
+		} catch (error) {
+			if (error.name == 'SyntaxError') {
+				let backup = JSON.parse(await fsp.readFile(fullPath.replace(/\.json$/, '.backup')));
+				await this.write(dataPath, backup, true);
+				return backup;
+			}
+		}
 	},
 	remove(dataPath, removeBackup = false) {
 		dataPath = addJson(dataPath);
 		let fullPath = path.join(this.basePath, dataPath);
-	    fsp.unlink(fullPath);
+		fsp.unlink(fullPath);
 		if (removeBackup) {
-		    fsp.unlink(fullPath.replace(/\.json$/, '.backup'));
+			fsp.unlink(fullPath.replace(/\.json$/, '.backup'));
 		}
 	},
 	async update(dataPath, data) {
 		dataPath = addJson(dataPath);
 		let fullPath = path.join(this.basePath, dataPath);
 		if (!fs.existsSync(fullPath)) {
-		    this.write(dataPath, data);
+			this.write(dataPath, data);
 		} else {
 			let dbData = await this.get(dataPath);
 			if (typeof dbData != 'object' && dbData !== null) {
@@ -43,13 +51,13 @@ module.exports = {
 			}
 		}
 	},
-	write(dataPath, data, overwrite = false) {
+	async write(dataPath, data, overwrite = false) {
 		dataPath = addJson(dataPath);
 		let fullPath = path.join(this.basePath, dataPath);
 		if (fs.existsSync(fullPath) && !overwrite) {
 			console.error("File already exists. To overwrite this file, set 'overwrite' equal to 'true'.");
 		} else {
-		  	fsp.writeFile(fullPath, JSON.stringify(data));
+			await fsp.writeFile(fullPath, JSON.stringify(data));
 		}
 	},
 }
